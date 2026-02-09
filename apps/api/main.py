cbce3cb33a8547a9f144603adf2c8f69cc09445a
Ethan Goodman <ecgoodm2@ncsu.edu>
2026-02-09 00:52:02 -0500

    Updated allowed domains

--- apps/api/main.py
+++ apps/api/main.py
@@ -31,6 +31,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://thrift-buddy.vercel.app",
+        "https://thriftbuddy.app",
+        "https://thriftpal.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
